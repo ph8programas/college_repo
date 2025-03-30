4fa0bde07a1b3cda7b97958a1a756d60011a7c94
Pedro-Windows <pedro8programas@gmail.com>
2025-03-30 11:36:14 -0300

    ead4_e3_fretes done

--- ifsul/bcc/semestre1/alg1/listas_exercicio/aula5_ead4/ead4_e3_fretes.cpp
+++ ifsul/bcc/semestre1/alg1/listas_exercicio/aula5_ead4/ead4_e3_fretes.cpp
@@ -1,17 +1,18 @@
 // Autor: Pedro H.C Rossetto
-// Data: 29/03/2025
+// Data: 30/03/2025
 // Descrição: Código para verificar ano bissexto
 // Lista EAD 4, Exercício 3, Algoritimos 1
 
 
 #include <iostream>
-#include <string>
+#include <cstdio>
+#include <iomanip>
 using namespace std;
 
 int main() {
-
-    int truck_year, cargo_code, state_code, cargo_kg_value, ir_quota, truck_age;
-    float cargo_ton_weight, cargo_total_value;
+    cout << fixed << setprecision(2);
+    int truck_year, cargo_code, state_code, cargo_kg_value, truck_age;
+    float cargo_ton_weight, cargo_value, ir_quota;
 
     cout << "Bem vindo(a) à calculadora de frete!" << endl << endl;
     cout << "Informe as informações solicitadas..." << endl << endl;
@@ -35,17 +36,17 @@ int main() {
 
     if (cargo_code >= 10 && cargo_code <= 20) {
         cargo_kg_value = 100;
-        cargo_total_value = cargo_ton_weight * (cargo_kg_value * 1000);
+        cargo_value = cargo_ton_weight * (cargo_kg_value * 1000);
     }
     else if (cargo_code >= 21 && cargo_code <= 30)
     {
         cargo_kg_value = 250;
-        cargo_total_value = cargo_ton_weight * (cargo_kg_value * 1000);
+        cargo_value = cargo_ton_weight * (cargo_kg_value * 1000);
     }
     else if (cargo_code >= 31 && cargo_code <= 40)
     {
         cargo_kg_value = 340;
-        cargo_total_value = cargo_ton_weight * (cargo_kg_value * 1000);
+        cargo_value = cargo_ton_weight * (cargo_kg_value * 1000);
     }
     
     do {cout << "Informe o código do estado (1-5): " << endl;
@@ -58,6 +59,12 @@ int main() {
     if (state_code == 4){ir_quota = ir_quota + 0.05;}
     if (state_code == 5){ir_quota = 0;}
     
-    cout << state_code << endl;
-    cout << truck_age;
+    float cargo_total_value, ir_value;
+    ir_value = cargo_value * ir_quota;
+    cargo_total_value = cargo_value + ir_value;
+    cout << "Tempo de uso do caminhão: "<< truck_age << " anos." << endl;
+    cout << "Peso da carga do caminhão: " << cargo_ton_weight * 1000 << " Kg" << endl;
+    cout << "Preço da carga: R$ " << cargo_value << endl;
+    cout << "Valor do Imposto: R$ " << ir_value << endl;
+    cout << "Valor total (Carga + Imposto): R$ " << cargo_total_value << endl;
 }
