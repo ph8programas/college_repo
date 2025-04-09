5a2ee3f19e82f4e009c2a408ee71a201054eb121
Pedro-Linux <pedro8programas@gmail.com>
2025-04-09 09:59:33 -0300

    css/html separation

---
+++ ifsul/bcc/semestre1/alg1/listas_exercicio/aula7_ead6/ead6_e3_menu.cpp
@@ -0,0 +1,23 @@
+// Autor: Pedro H.C Rossetto
+// Data: 07/04/2025
+// Descrição: Código para para apresentar menu
+// Lista de Exercícios, Exercício 1, EAD6
+
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main () {
+    int option;
+    do {
+        cout << "Selecione uma opção:\n"
+             << "0 - Sair\n"
+             << "1 - Ler um Valor\n"
+             << "2 - Mostrar os números de 1 até o valor lido\n"
+             << "3 - Verificar se o número lido é par ou impar\n";
+             cin >> option;
+    } while ()
+}
