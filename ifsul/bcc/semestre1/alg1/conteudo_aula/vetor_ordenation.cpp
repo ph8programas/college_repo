4d8a537e117721e8039a6d17df80216a6b18c641
IFSUL PC <pedro8programas@gmail.com>
2025-05-29 08:34:48 -0300

    beggining trabalho

---
+++ ifsul/bcc/semestre1/alg1/conteudo_aula/vetor_ordenation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main(){
+    const int TAM = 10;
+    int vet[TAM] = {2,3,1,4,6,5,7,8,9,0};
+    int i,j,min,aux;
+
+    cout <<"\n Valores do vetor:\n";
+
+}
---
+++ ifsul/bcc/semestre1/alg1/trabalho_pratico2/main.cpp
@@ -0,0 +1,36 @@
+// Autor: Pedro H.C Rossetto
+// Data: 29/03/2025
+// Descrição: Código para simular controle bancário
+// Trabalho prático 2, exercicio unico
+
+#include <cstdio>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void print_menu(){
+    cout << "\n----------- MENU -----------\n";
+    cout << "0. Inicializar Vetores Conta e Saldo\n";
+    cout << "1. Efetuar depósito\n";
+    cout << "2. Efetuar saque\n";
+    cout << "3. Consultar o ativo bancário\n";
+    cout << "4. Finalizar o programa\n";
+    return;
+}
+
+
+int cadastrar_conta(vector vet, int conta)
+
+
+int main(){
+
+int TAMANHO_VETOR = 10;
+int contas[TAMANHO_VETOR];
+float saldos[TAMANHO_VETOR];
+
+print_menu();
+
+
+
+
+}
